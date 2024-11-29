9ce3919bc49572db03e2165952fdf7e2bb449958
nathanhenriquesilva <nathhenrique2007@gmail.com>
2024-11-29 16:33:41 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<h2>receita:
+    <?php
+    echo "brigadeiro"
+    ?>
+    </h2>
+    <table border = "1">
+        <thead>
+            <tr>
+                <th>ingredientes</th>
+                <th>quantidade</th>
+</tr>
+</thead>
+<tboody>
+    <tr>
+        <td>leite condensado</td>
+        <td>
+        1 caixa
+        </td>
+    </tr>
+    <td>margarina sem sal</td>
+    <td>1 colher</td>
+    <tr>
+        <td>achocolatado</td>
+        <td>7 colheres</td>
+    </tr>
+    <tr>
+        <td>granulado</td>
+        <td>a gosto</td>
+    </tr>
+</table>
+    <h1>nota: 10</h1>
+    <script>
+        var h1= document.querySelector("h1");
+        h1.innerHTML="nota 10";
+    </script>
+    
